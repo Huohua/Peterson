01d96ca6fabb5a407831006540d3ef3b7ba9dfa9
Zheng Sun <sunzheng.david@gmail.com>
2013-02-26 15:52:02 +0800

    Update weibo sharing

--- src/tv/huohua/peterson/social/WeiboAccessTokenKeeper.java
+++ src/tv/huohua/peterson/social/WeiboAccessTokenKeeper.java
@@ -6,24 +6,10 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 
-/**
- * 该类用于保存Oauth2AccessToken到sharepreference，并提供读取功能
- * 
- * @author xiaowei6@staff.sina.com.cn
- * 
- */
-public class AccessTokenKeeper {
+public class WeiboAccessTokenKeeper {
     private static final String PREFERENCES_NAME = "com_weibo_sdk_android";
 
-    /**
-     * 保存accesstoken到SharedPreferences
-     * 
-     * @param context
-     *            Activity 上下文环境
-     * @param token
-     *            Oauth2AccessToken
-     */
-    public static void keepAccessToken(Context context, Oauth2AccessToken token) {
+    public static void keepAccessToken(final Context context, final Oauth2AccessToken token) {
         SharedPreferences pref = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_APPEND);
         Editor editor = pref.edit();
         editor.putString("token", token.getToken());
@@ -31,11 +17,6 @@ public class AccessTokenKeeper {
         editor.commit();
     }
 
-    /**
-     * 清空sharepreference
-     * 
-     * @param context
-     */
     public static void clear(Context context) {
         SharedPreferences pref = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_APPEND);
         Editor editor = pref.edit();
@@ -43,12 +24,6 @@ public class AccessTokenKeeper {
         editor.commit();
     }
 
-    /**
-     * 从SharedPreferences读取accessstoken
-     * 
-     * @param context
-     * @return Oauth2AccessToken
-     */
     public static Oauth2AccessToken readAccessToken(Context context) {
         Oauth2AccessToken token = new Oauth2AccessToken();
         SharedPreferences pref = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_APPEND);
