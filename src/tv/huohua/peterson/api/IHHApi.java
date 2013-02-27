d8bed021a6f25e8c4b213a0dc91796d7e893096e
Zheng Sun <sunzheng.david@gmail.com>
2013-02-27 21:58:15 +0800

    Add license

--- src/tv/huohua/peterson/api/IHHApi.java
+++ src/tv/huohua/peterson/api/IHHApi.java
@@ -1,9 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Zheng Sun.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Zheng Sun - initial API and implementation
+ ******************************************************************************/
+
 package tv.huohua.peterson.api;
 
 import java.io.Serializable;
 
 import android.content.Context;
 
-public interface IHHApi extends Serializable{
-	ApiCallResponse call(Context context);
+public interface IHHApi extends Serializable {
+    ApiCallResponse call(Context context);
 }
