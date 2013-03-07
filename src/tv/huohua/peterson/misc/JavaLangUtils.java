b3252d01870e8b423fad38db1cd8cc9916ea1207
Zheng Sun <sunzheng.david@gmail.com>
2013-03-07 14:07:56 +0800

    Extend JavaLangUtils

--- src/tv/huohua/peterson/misc/JavaLangUtils.java
+++ src/tv/huohua/peterson/misc/JavaLangUtils.java
@@ -12,6 +12,18 @@
 package tv.huohua.peterson.misc;
 
 final public class JavaLangUtils {
+    public static String implode(final int[] input, final String seperator) {
+        final StringBuilder builder = new StringBuilder();
+        if (input.length > 0) {
+            builder.append(input[0]);
+            for (int i = 1; i < input.length; i++) {
+                builder.append(seperator);
+                builder.append(input[i]);
+            }
+        }
+        return builder.toString();
+    }
+    
     public static String implode(final Object[] input, final String seperator) {
         final StringBuilder builder = new StringBuilder();
         if (input.length > 0) {
