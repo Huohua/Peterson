320f5b3eaa4f2da26e1aa1c8dc7284276c6ba68d
Zheng Sun <sunzheng.david@gmail.com>
2013-04-17 10:12:57 +0800

    Update NetworkUtils

--- src/tv/huohua/peterson/network/NetworkUtils.java
+++ src/tv/huohua/peterson/network/NetworkUtils.java
@@ -13,7 +13,6 @@ package tv.huohua.peterson.network;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
 import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,8 +20,15 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.GzipDecompressingEntity;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
@@ -32,14 +38,41 @@ import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
 final public class NetworkUtils {
+    final private static DefaultHttpClient HTTP_CLIENT;
     static {
-        HttpURLConnection.setFollowRedirects(true);
+        HTTP_CLIENT = new DefaultHttpClient();
+        HTTP_CLIENT.addRequestInterceptor(new HttpRequestInterceptor() {
+            @Override
+            public void process(org.apache.http.HttpRequest request, HttpContext context) throws HttpException,
+                    IOException {
+                if (!request.containsHeader("Accept-Encoding")) {
+                    request.addHeader("Accept-Encoding", "gzip");
+                }
+            }
+        });
+        HTTP_CLIENT.addResponseInterceptor(new HttpResponseInterceptor() {
+            public void process(final HttpResponse response, final HttpContext context) throws HttpException,
+                    IOException {
+                final HttpEntity entity = response.getEntity();
+                final Header encodingHeader = entity.getContentEncoding();
+                if (encodingHeader != null) {
+                    final HeaderElement[] codecs = encodingHeader.getElements();
+                    for (int i = 0; i < codecs.length; i++) {
+                        if (codecs[i].getName().equalsIgnoreCase("gzip")) {
+                            response.setEntity(new GzipDecompressingEntity(response.getEntity()));
+                            return;
+                        }
+                    }
+                }
+            }
+        });
     }
 
     public static Map<String, List<String>> getQueryParams(final String url) {
@@ -71,8 +104,7 @@ final public class NetworkUtils {
     }
 
     public static HttpResponse httpQuery(final HttpRequest request) throws ClientProtocolException, IOException {
-        final DefaultHttpClient client = new DefaultHttpClient();
-        final HttpParams params = client.getParams();
+        final HttpParams params = HTTP_CLIENT.getParams();
         HttpConnectionParams.setConnectionTimeout(params, 40 * 1000);
         HttpConnectionParams.setSoTimeout(params, 30 * 1000);
 
@@ -128,7 +160,7 @@ final public class NetworkUtils {
                 requestBase.setHeader(kv.getKey(), kv.getValue());
             }
         }
-        return client.execute(requestBase);
+        return HTTP_CLIENT.execute(requestBase);
     }
 
     public static boolean isNetworkAvailable(Context context) {
