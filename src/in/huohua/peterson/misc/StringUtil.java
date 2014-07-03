6ac13bb7c4a7a9b6c520dc22412352c2f4fbfe64
gongming <gongmingqm10@foxmail.com>
2014-07-03 22:54:10 +0800

    avoid NullPointerException in StringUtil

--- src/in/huohua/peterson/misc/StringUtil.java
+++ src/in/huohua/peterson/misc/StringUtil.java
@@ -1,8 +1,11 @@
 package in.huohua.peterson.misc;
 
+import android.text.TextUtils;
+
 public class StringUtil {
 	static public String implode(String[] strings, String glue) {
 		StringBuilder stringBuilder = new StringBuilder();
+        if (strings == null) return "";
 		for (int i = 0; i < strings.length; i++) {
 			stringBuilder.append(strings[i]);
 		    if (i != strings.length - 1) {
