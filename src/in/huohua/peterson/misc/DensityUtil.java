cea148f390ccfbd457a7d9fc22bda5238781a93c
mzule <Lennnna@LennnnatekiMacBook-Air.local>
2014-10-23 18:00:27 +0800

    @fuluchii DensityUtil with if null check

--- src/in/huohua/peterson/misc/DensityUtil.java
+++ src/in/huohua/peterson/misc/DensityUtil.java
@@ -4,11 +4,17 @@ import android.content.Context;
 
 public class DensityUtil {
     public static int dip2px(Context context, float dpValue) {
+        if (context == null) {
+            return 0;
+        }
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
     }
 
     public static int px2dip(Context context, float pxValue) {
+        if (context == null) {
+            return 0;
+        }
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
     }
